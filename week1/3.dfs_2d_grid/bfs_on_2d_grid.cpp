963a203812352328f51cad672f22507895a4f91e
developertanvir2019 <developertanvir2019@gmail.com>
2024-09-05 15:13:19 +0600

    practice dfs

---
+++ week1/3.dfs_2d_grid/bfs_on_2d_grid.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool vis[20][20];
+int dis[20][20];
+ int n,m;
+ char a[20][20];
+vector<pair<int,int>>d={{0,1},{1,-1},{-1,0},{1,0}};
+
+bool valid(int i,int j){
+if(i<0 ||i>=n ||j<0||j>=m){
+return false;
+return true;
+}
+}
+void bfs(int si,int sj){
+queue<pair<int,int>>q;
+q.push({si,sj});
+vis[si][sj]=true;
+while(!q.empty()){
+    pair<int,int> par=q.front();
+    q.pop();
+    cout<<par.first<<" "<<par.second<<endl;
+    for(int i=0;i<4;i++){
+        int ci=par.first+d[i].first;
+        int cj=par.second+d[i].second;
+        if(valid(ci,cj)&&!vis[ci][cj]){
+        q.push({ci,cj});
+        vis[ci][cj]=true;
+        dis[ci][cj]=dis[par.first][par.second]+1;
+        }
+    }
+   
+}
+}
+int main (){
+      cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+    int si,sj;
+    cin>>si>>sj;
+    memset(vis,false,sizeof(vis)); 
+    memset(dis,false,sizeof(dis)); 
+    bfs(si,sj);
+    return 0;
+}
