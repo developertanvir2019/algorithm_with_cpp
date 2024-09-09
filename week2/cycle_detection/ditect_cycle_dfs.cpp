a53c46ba104db8f1e9bcefd829fc32079b317f5a
developertanvir2019 <developertanvir2019@gmail.com>
2024-09-09 15:59:40 +0600

    practice dijkstra algorithm

---
+++ week2/cycle_detection/ditect_cycle_dfs.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=1e5+5;
+bool vis[N];
+vector<int>adj[N];
+int parArray[N];
+bool ans;
+void dfs(int parent){
+    vis[parent]=true;
+    for(int child:adj[parent]){
+
+        if(vis[child] &&child!=parArray[parent]){
+        ans =true;
+        }
+        if(!vis[child]){
+            parArray[child]=parent;
+            dfs(child);
+        }
+    } 
+}
+int main (){
+    int n,e;
+    cin>>n>>e;
+    while(e--){
+        int a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    memset(vis,false,sizeof(vis));
+    memset(parArray,-1,sizeof(parArray));
+    ans=false;
+
+    for(int i=0;i<n;i++){
+       if(!vis[i]){
+        dfs(i);
+       }
+    }
+
+    if(ans){
+        cout<<"Cycle found";
+    }else cout<<"Cycle Not found";
+    return 0;
+} 
---
+++ week2/dijkstra/dijkstra_optimized.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=100;
+vector<pair<int,int>> v[N];
+int dis[N];
+
+class cmp{
+    public:
+    bool operator()(pair<int,int>a,pair<int,int>b){
+        return a.second>b.second;
+    }
+};
+
+void dijkstra(int src){
+ priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
+    pq.push({src,0});
+    dis[src]=0;
+    while(!pq.empty()){
+     pair<int,int>parent=pq.top();
+     pq.pop();
+     int node=parent.first;
+     int cost=parent.second;
+     for(pair<int,int>child:v[node]){
+        int childNode=child.first;
+        int childCost=child.second;
+        if(cost+childCost<dis[childNode]){
+            dis[childNode]=cost+childCost;
+            pq.push({childNode,cost+childCost});
+        }
+     }
+    }
+}
+int main (){
+    int n,e;
+    cin>>n>>e;
+    while(e--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        v[a].push_back({b,c});
+        v[b].push_back({a,c});
+
+    }
+    // memset(dis,INT_MAX,sizeof(dis));
+// memset alternative
+for(int i=0;i<n;i++){
+    dis[i]=INT_MAX;
+}
+
+    dijkstra(0);
+    return 0;
+}
